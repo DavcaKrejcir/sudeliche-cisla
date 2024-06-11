57d5f2323bb21f52335ec424f89e89dcb4496019
krejcir.david <krejcir.david@D20-10>
2024-06-11 11:23:06 +0200

    ZMENA-DALSI

--- sudeliche-cisla/Program.vb
+++ sudeliche-cisla/Program.vb
@@ -1,18 +1,18 @@
 Module JednoduchyVstup
     Sub Main()
         Dim pocet As Integer
-        Console.Write("Zadejte poèet èísel: ")
-        While Not Integer.TryParse(Console.ReadLine(), pocet)
-            Console.WriteLine("Neplatný vstup. Zadejte prosím celé èíslo.")
-            Console.Write("Zadejte poèet èísel: ")
+        Console.Write("Zadejte kladný poèet èísel: ")
+        While Not (Integer.TryParse(Console.ReadLine(), pocet) AndAlso pocet > 0)
+            Console.WriteLine("Neplatný vstup. Zadejte prosím kladné celé èíslo.")
+            Console.Write("Zadejte kladný poèet èísel: ")
         End While
 
         Dim cislo As Integer
         For i As Integer = 1 To pocet
-            Console.Write("Zadejte èíslo " & i & ": ")
-            While Not Integer.TryParse(Console.ReadLine(), cislo)
-                Console.WriteLine("Neplatný vstup. Zadejte prosím celé èíslo.")
-                Console.Write("Zadejte èíslo " & i & ": ")
+            Console.Write("Zadejte kladné èíslo " & i & ": ")
+            While Not (Integer.TryParse(Console.ReadLine(), cislo) AndAlso cislo >= 0)
+                Console.WriteLine("Neplatný vstup. Zadejte prosím kladné celé èíslo nebo nulu.")
+                Console.Write("Zadejte kladné èíslo " & i & ": ")
             End While
         Next
 
